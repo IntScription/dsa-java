b447d3ebdbb684a67cc72c880bb254d385391b24
Kartik Sanil <22kartiksanil@gmail.com>
2025-09-15 16:21:50 +0530

    Arrays

--- Practice/array.java
+++ Practice/array.java
@@ -3,5 +3,14 @@ public class array {
     int[] nums = { 1, 2, 3, 4, 5 }; // array with initial values
     System.out.println(nums[0]); // prints 1
     System.out.println(nums[4]); // prints 5
+
+    // Traversing Arrays
+    for (int i = 0; i < nums.length; i++) {
+      System.out.println(nums[i] + " ");
+    }
+
+    for (int n : nums) { // enhanced for-loop
+      System.out.println(n + " ");
+    }
   }
 }
