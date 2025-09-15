ae83d1a3834d339148753d606efffcbf928c0b40
Kartik Sanil <22kartiksanil@gmail.com>
2025-09-15 14:46:44 +0530

    Array

---
+++ Practice/array.java
@@ -0,0 +1,7 @@
+public class array {
+  public static void main(String args[]) {
+    int[] nums = { 1, 2, 3, 4, 5 }; // array with initial values
+    System.out.println(nums[0]); // prints 1
+    System.out.println(nums[4]); // prints 5
+  }
+}
