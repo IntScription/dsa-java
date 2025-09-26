f33f8a63114dabb00e818e3fa69d54028972012f
Kartik Sanil <22kartiksanil@gmail.com>
2025-09-26 15:46:51 +0530

    while loops

---
+++ Practice/loop.java
@@ -0,0 +1,10 @@
+public class loop {
+  public static void main(String[] args) {
+    int x = 5, y = 1;
+
+    while (x > y) {
+      System.out.println("Hello");
+      x--; // x = x - 1;
+    }
+  }
+}
